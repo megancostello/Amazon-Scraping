04617474b082d274990c5c40d0447d9c818e3c8c
Megan Costello <megancostello@users.noreply.github.com>
2020-08-11 00:19:37 -0700

    Fixed Brand scrape
    Removes 'Visit the ______ Store' so that it logs just the brand name, not the whole phrase. Also added a tryCount variable that only lets the script request the same URL 5 times before giving up and moving on to the next time - lets the program finish even if it encounters a broken link that it can't scrape the title from.

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
-import json
 
 workbook = load_workbook(filename="Log.xlsx")
 sheet = workbook["Logged"]
@@ -22,117 +21,125 @@ for urlRow in range(2, workbook["Actual"].max_row+1):
     soup = getData(url)
     title = ''
     hasTitle = False
+    tryCount = 5
     while hasTitle is not True:
         try:
             title = soup.find(id="productTitle").get_text().rstrip()
             hasTitle = True
             print('soup is good')
         except AttributeError or TypeError:
+            if tryCount == 0:
+                break
             soup = getData(url)
+            tryCount-=1
             print('soup redone')
 
-    brand = soup.find(id="bylineInfo").get_text().rstrip()
+    if tryCount != 0:
 
-    styleCount = 0
-    baseStyleName = ''
-    dropdownStyle = False
-    if soup.find(id="native_dropdown_selected_style_name") is not None:
-        baseStyleName = "native_style_name_"
-        dropdownStyle=True
-    else:
-        baseStyleName = "style_name_"
-    styleName = baseStyleName + str(styleCount)
+        brand = soup.find(id="bylineInfo").get_text().rstrip()
+        brand = brand.replace('Visit the ', '')
+        brand = brand.replace(' Store', '')
 
-    while soup.find(id=styleName):
-        styleCount+=1
+        styleCount = 0
+        baseStyleName = ''
+        dropdownStyle = False
+        if soup.find(id="native_dropdown_selected_style_name") is not None:
+            baseStyleName = "native_style_name_"
+            dropdownStyle=True
+        else:
+            baseStyleName = "style_name_"
         styleName = baseStyleName + str(styleCount)
 
-    hasStyles = False
-    if styleCount > 0:
-        hasStyles = True
+        while soup.find(id=styleName):
+            styleCount+=1
+            styleName = baseStyleName + str(styleCount)
 
-    sizeCount = 0
-    baseSizeName = "size_name_"
-    sizeName = baseSizeName + str(sizeCount)
+        hasStyles = False
+        if styleCount > 0:
+            hasStyles = True
 
-    while soup.find(id=sizeName):
-        sizeCount+=1
+        sizeCount = 0
+        baseSizeName = "size_name_"
         sizeName = baseSizeName + str(sizeCount)
 
-    hasSizes = False
-    if sizeCount > 0:
-        hasSizes = True
+        while soup.find(id=sizeName):
+            sizeCount+=1
+            sizeName = baseSizeName + str(sizeCount)
 
-    colorCount = 0
-    baseColorName = "color_name_"
-    colorName = baseColorName + str(colorCount)
+        hasSizes = False
+        if sizeCount > 0:
+            hasSizes = True
 
-    while soup.find(id=colorName):
-        colorCount+=1
+        colorCount = 0
+        baseColorName = "color_name_"
         colorName = baseColorName + str(colorCount)
 
-    hasColors = False
-    if colorCount > 0:
-        hasColors = True
+        while soup.find(id=colorName):
+            colorCount+=1
+            colorName = baseColorName + str(colorCount)
 
-    selectedStyle = ''
-    selections = []
+        hasColors = False
+        if colorCount > 0:
+            hasColors = True
 
-    if dropdownStyle:
-        selectedStyle = soup.find(id="dropdown_selected_style_name").get_text().rstrip()
-        print(selectedStyle)
-        sheet.cell(row=rowCounter, column=6).value = selectedStyle
-        hasStyles = False
+        selectedStyle = ''
+        selections = []
 
-    allSelections = soup.findAll("span", attrs={"class": "selection"})
+        if dropdownStyle:
+            selectedStyle = soup.find(id="dropdown_selected_style_name").get_text().rstrip()
+            print(selectedStyle)
+            sheet.cell(row=rowCounter, column=6).value = selectedStyle
+            hasStyles = False
 
-    for selection in allSelections:
-        temp = selection.get_text().rstrip()
-        temp2 = temp.replace("\n","")
-        selections.append(temp2)
+        allSelections = soup.findAll("span", attrs={"class": "selection"})
 
-    if hasStyles and hasSizes and hasColors:
-        sheet.cell(row=rowCounter, column=6).value = selections[0]
-        sheet.cell(row=rowCounter, column=7).value = selections[1]
-        sheet.cell(row=rowCounter, column=8).value = selections[2]
-    elif hasStyles and hasSizes and not hasColors:
-        sheet.cell(row=rowCounter, column=7).value = selections[0]
-        sheet.cell(row=rowCounter, column=6).value = selections[1]
-        sheet.cell(row=rowCounter, column=8).value = None
-    elif not hasStyles and hasSizes and hasColors:
-        sheet.cell(row=rowCounter, column=7).value = selections[0]
-        sheet.cell(row=rowCounter, column=8).value = selections[1]
-        if not dropdownStyle:
-            sheet.cell(row=rowCounter, column=6).value = None
-    elif hasStyles and not hasSizes and hasColors:
-        sheet.cell(row=rowCounter, column=6).value = selections[0]
-        sheet.cell(row=rowCounter, column=8).value = selections[1]
-        sheet.cell(row=rowCounter, column=7).value = None
-    elif hasStyles and not hasSizes and not hasColors:
-        sheet.cell(row=rowCounter, column=6).value = selections[0]
-        sheet.cell(row=rowCounter, column=7).value = None
-        sheet.cell(row=rowCounter, column=8).value = None
-    elif not hasStyles and hasSizes and not hasColors:
-        sheet.cell(row=rowCounter, column=7).value = selections[0]
-        sheet.cell(row=rowCounter, column=8).value = None
-        if not dropdownStyle:
-            sheet.cell(row=rowCounter, column=6).value = None
-    elif not hasStyles and not hasSizes and hasColors:
-        sheet.cell(row=rowCounter, column=8).value = selections[0]
-        sheet.cell(row=rowCounter, column=7).value = None
-        if not dropdownStyle:
-            sheet.cell(row=rowCounter, column=6).value = None
-    print(selections)
+        for selection in allSelections:
+            temp = selection.get_text().rstrip()
+            temp2 = temp.replace("\n","")
+            selections.append(temp2)
 
-    for char in title:
-        if (char == '\n'):
-           title = title.replace(char, '')
+        if hasStyles and hasSizes and hasColors:
+            sheet.cell(row=rowCounter, column=6).value = selections[0]
+            sheet.cell(row=rowCounter, column=7).value = selections[1]
+            sheet.cell(row=rowCounter, column=8).value = selections[2]
+        elif hasStyles and hasSizes and not hasColors:
+            sheet.cell(row=rowCounter, column=7).value = selections[0]
+            sheet.cell(row=rowCounter, column=6).value = selections[1]
+            sheet.cell(row=rowCounter, column=8).value = None
+        elif not hasStyles and hasSizes and hasColors:
+            sheet.cell(row=rowCounter, column=7).value = selections[0]
+            sheet.cell(row=rowCounter, column=8).value = selections[1]
+            if not dropdownStyle:
+                sheet.cell(row=rowCounter, column=6).value = None
+        elif hasStyles and not hasSizes and hasColors:
+            sheet.cell(row=rowCounter, column=6).value = selections[0]
+            sheet.cell(row=rowCounter, column=8).value = selections[1]
+            sheet.cell(row=rowCounter, column=7).value = None
+        elif hasStyles and not hasSizes and not hasColors:
+            sheet.cell(row=rowCounter, column=6).value = selections[0]
+            sheet.cell(row=rowCounter, column=7).value = None
+            sheet.cell(row=rowCounter, column=8).value = None
+        elif not hasStyles and hasSizes and not hasColors:
+            sheet.cell(row=rowCounter, column=7).value = selections[0]
+            sheet.cell(row=rowCounter, column=8).value = None
+            if not dropdownStyle:
+                sheet.cell(row=rowCounter, column=6).value = None
+        elif not hasStyles and not hasSizes and hasColors:
+            sheet.cell(row=rowCounter, column=8).value = selections[0]
+            sheet.cell(row=rowCounter, column=7).value = None
+            if not dropdownStyle:
+                sheet.cell(row=rowCounter, column=6).value = None
+        print(selections)
 
-    sheet.cell(row=rowCounter, column=1).value = title
-    sheet.cell(row=rowCounter, column=2).value = brand
-    sheet.cell(row=rowCounter, column=3).value = styleCount
-    sheet.cell(row=rowCounter, column=4).value = sizeCount
-    sheet.cell(row=rowCounter, column=5).value = colorCount
+        for char in title:
+            if (char == '\n'):
+               title = title.replace(char, '')
+
+        sheet.cell(row=rowCounter, column=1).value = title
+        sheet.cell(row=rowCounter, column=2).value = brand
+        sheet.cell(row=rowCounter, column=3).value = styleCount
+        sheet.cell(row=rowCounter, column=4).value = sizeCount
+        sheet.cell(row=rowCounter, column=5).value = colorCount
 
     for x in range(1,9):
         if sheet.cell(row=rowCounter, column=x).value != workbook["Actual"].cell(row=rowCounter, column=x).value:
